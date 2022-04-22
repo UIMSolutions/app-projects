2eae0627b6899c8a134d21ecdf71cad74fc32dbf
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-04-22 07:09:39 +0200

    Corrections

--- source/apps/projects/package.d
+++ source/apps/projects/package.d
@@ -1,6 +1,27 @@
 module apps.projects;
 
 @safe:
+mixin ImportPhobos;
+mixin ImportDubs;
+mixin ImportUim;
+
+public import colored;
+
+// Additional imports
+public import uim.systems;
+public import uim.jsonbase;
+public import uim.entities;
+public import uim.entitybase;
+
+public import uim.projects;
+
+// server-modeller packages
+public import servers.modeller;
+
+public import models.systems;
+public import models.projects;
+
+public import layouts.tabler;
 
 public import apps.projects.controllers;
 public import apps.projects.routers;
